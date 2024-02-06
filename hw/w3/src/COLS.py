32e7fa18343e3ebd4adea53b12addac58b9da152
syeddul <syeddul@ncsu.edu>
2024-02-06 10:09:58 -0500

    merge fix

--- hw/w3/src/COLS.py
+++ hw/w3/src/COLS.py
@@ -1,24 +1,5 @@
 from SYM import SYM
 from NUM import NUM
-<<<<<<< HEAD
-class COLS:
-    def __init__(self, row):
-        self.x, self.y, self.all = {}, {}, []
-        self.klass = None
-        col=None
-        for at, txt in enumerate(row.cells):
-            col = (NUM if txt[0].isupper() else SYM)()  # Instantiate NUM or SYM directly
-            col.txt = txt
-            col.at = at
-            self.all.append(col)
-            if not txt.endswith('X'):
-                if txt.endswith('!'):
-                    self.klass = col
-                if txt.endswith("!") or txt.endswith("+") or txt.endswith("-"):
-                    self.y[at]=col
-                else:
-                    self.x[at]=col
-=======
 
 
 class COLS:
@@ -42,7 +23,6 @@ class COLS:
                 self.y[at] = col
             else:
                 self.x[at] = col
->>>>>>> 2ec04a716ad72c88cce8920bf6f9e04a8b963931
         self.names = row.cells
         
 
