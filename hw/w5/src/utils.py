74d1faca4f8d331e2a2eb12c3c9a0d16ca7a2c43
Krishna Chaithanya <chaitanyam.be20@uceou.edu>
2024-02-22 20:26:28 -0500

    feat(hw5): num,row,sym and utils changed

---
+++ hw/w5/src/utils.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import ast
+
+
+def coerce(x):
+    try:
+        return ast.literal_eval(x)
+    except Exception:
+        return x.strip()
+
+
+def csv(sFilename, fun):
+    fileDescriptor = Path(sFilename)
+    if fileDescriptor.exists() == False:
+        raise FileNotFoundError()
+    if fileDescriptor.suffix != ".csv":
+        raise Exception("data file should be csv")
+    with open(fileDescriptor.absolute(), "r", encoding="utf-8") as file:
+        for line in file:
+            row = list(map(coerce, line.strip().split(",")))
+            fun(row)
+
+
+def o(t, n=None, u=None):
+    if isinstance(t, (int, float)):
+        return str(rounding(t, n))
+    if not isinstance(t, dict):
+        return str(t)
+    if u is None:
+        u = []
+    for k in t.keys():
+        if str(k)[0] != "_":
+            if len(t) > 0:
+                u.append(o(t[k], n))
+            else:
+                u.append(f"{o(k, n)}: {o(t[k], n)}")
+    return "{" + ", ".join(u) + "}"
+
+def rounding(n, ndecs=None):
+    if not isinstance(n, (int, float)):
+        return n
+    if math.floor(n) == n:
+        return n
+    mult = 10**(ndecs or 2)
+    return math.floor(n * mult + 0.5) / mult
+
+
